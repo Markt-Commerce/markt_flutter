e0e3a26c24fe224172cc1e849cf530b609fa2481
amethystcoder <amethystguitaristj@gmail.com>
2024-01-08 22:46:35 +0000

    new productpack methods

--- markt/lib/services/productpackservice.dart
+++ markt/lib/services/productpackservice.dart
@@ -4,12 +4,38 @@ import 'package:http/http.dart' as http;
 class ProductPackService {
   
   ProductPackService();
+
+  final productBaseUrl = '';
   
-  getRandomProducts(){}
+  Future<List<Map<String,dynamic>>> getRandomProducts()async {
+    http.Response response = await http.get(Uri.parse(productBaseUrl));
+    if (response.statusCode > 300) {
+      throw Exception('cannot get products${response.statusCode}');
+    }
+    return jsonDecode(response.body);
+  }
 
-  getProducts(){}
+  Future<List<Map<String,dynamic>>> getProducts() async{
+    http.Response response = await http.get(Uri.parse(productBaseUrl));
+    if (response.statusCode > 300) {
+      throw Exception('cannot get products${response.statusCode}');
+    }
+    return jsonDecode(response.body);
+  }
 
-  getProductsUsingCategory(){}
+  Future<List<Map<String,dynamic>>> getProductsUsingCategory() async{
+    http.Response response = await http.get(Uri.parse(productBaseUrl));
+    if (response.statusCode > 300) {
+      throw Exception('cannot get products${response.statusCode}');
+    }
+    return jsonDecode(response.body);
+  }
 
-  searchProduct(){}
+  Future<List<Map<String,dynamic>>> searchProduct()async{
+    http.Response response = await http.get(Uri.parse(productBaseUrl));
+    if (response.statusCode > 300) {
+      throw Exception('cannot get products${response.statusCode}');
+    }
+    return jsonDecode(response.body);
+  }
 }
