1412415bfb5aa752fae624019eee0c8e661be255
amethystcoder <amethystguitaristj@gmail.com>
2024-01-18 18:06:42 +0000

    some work on the buyer request section

--- markt/lib/models/buyerrequestmodel.dart
+++ markt/lib/models/buyerrequestmodel.dart
@@ -10,6 +10,28 @@ class BuyerRequests {
   late String category;
   late String staleTime;
 
-  BuyerRequests();
+  BuyerRequests({
+    required buyerName,
+    required city,
+    required state,
+    required profileImage,
+    required queryId,
+    required message,
+    required buyerId,
+    required category,
+    required staleTime,
+  });
+
+   Iterable<BuyerRequests> listFromJSON(List<Map<String,dynamic>> data){
+    return data.map((e) => BuyerRequests(buyerName: e["buyer_name"], city: e["city"], state: e["state"],
+     profileImage: e["profile_image"], queryId: e["query_id"], message: e["message"], 
+     buyerId: e["buyer_id"], category: e["category"], staleTime: e["stale_time"]));
+  }
+
+  factory BuyerRequests.fromJSON(Map<String,dynamic> data){
+    return BuyerRequests(buyerName: data["buyer_name"], city: data["city"], state: data["state"],
+     profileImage: data["profile_image"], queryId: data["query_id"], message: data["message"], 
+     buyerId: data["buyer_id"], category: data["category"], staleTime: data["stale_time"]);
+  }
 
 }
--- markt/lib/services/buyerrequestservice.dart
+++ markt/lib/services/buyerrequestservice.dart
@@ -1,6 +1,36 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 class BuyerRequestService {
 
   BuyerRequestService();
 
-  getBuyerRequests(){}
+  final buyerRequestUrl = "";
+
+  Future<List<Map<String,dynamic>>> getRequests(String category)async{
+    http.Response response  = await http.get(Uri.parse(buyerRequestUrl));
+    if (response.statusCode != 200) {
+      throw Exception("could not get requests. check your internet connection");
+    }
+    return jsonDecode(response.body);
+  }
+
+  Future<List<Map<String,dynamic>>> getBuyerRequests(String buyerId)async{
+    http.Response response  = await http.get(Uri.parse(buyerRequestUrl));
+    if (response.statusCode != 200) {
+      throw Exception("could not get buyer requests. check your internet connection");
+    }
+    return jsonDecode(response.body);
+  }
+
+  createBuyerRequest(Map<String,dynamic> data)async {
+    http.Response response  = await http.post(Uri.parse(buyerRequestUrl),body: data);
+    if (response.statusCode != 202) {
+      throw Exception("could not get buyer requests. check your internet connection");
+    }
+    return jsonDecode(response.body);
+  }
+
+  deleteBuyerRequest(Map<String,dynamic> data)async {}
+
 }
